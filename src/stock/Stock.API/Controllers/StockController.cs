648ac8e94e667449830dc1b55da80a47ed861f23
Ali Ergüç <ergucali1@gmail.com>
2023-07-24 15:51:43 +0300

    Create GetAllStocks Controller.

---
+++ src/stock/Stock.API/Controllers/StockController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Stock.Infrastructure.Context;
+
+namespace Stock.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public StockController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet("GetAllStocks")]
+        public async Task<IActionResult> GetAllStocks()
+        {
+
+            return Ok(await _appDbContext.OrderStocks.ToListAsync());
+        }
+    }
+}
