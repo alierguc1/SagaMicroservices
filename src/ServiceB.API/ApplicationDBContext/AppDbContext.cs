0bbd1a7ca98a2553125523f2fcfcfdc7a21ecb7e
Ali Ergüç <ergucali1@gmail.com>
2023-07-30 18:03:59 +0300

    Add PostgreSql implementation.

---
+++ src/ServiceB.API/ApplicationDBContext/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceB.API.Models;
+
+namespace ServiceB.API.ApplicationDBContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseNpgsql("User ID=postgres;Password=123456;Server=localhost;Port=5432;Database=ServiceB_DB;Integrated Security=true;Pooling=true;");
+
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ src/ServiceB.API/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ServiceB.API.Models
+{
+    public class Product
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public long ProductPrice { get; set; }
+    }
+}
