6d613d8dd25ef247c5b6999b3cd8234301fe4301
Ali Ergüç <ergucali1@gmail.com>
2023-07-31 19:04:07 +0300

    Add circuit breaker advanced and beginner implementation.

--- src/ServiceA.API/Program.cs
+++ src/ServiceA.API/Program.cs
@@ -2,6 +2,7 @@ using Polly.Extensions.Http;
 using Polly;
 using ServiceA.API;
 using System.Diagnostics;
+using Polly.CircuitBreaker;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +12,46 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//var circuitBreakerPolicy = GetCircuitBreakerPolicy();
+var circuitBreakerPolicy = GetAdvancedCircuitBreakerPolicy();
 builder.Services.AddHttpClient<ProductService>(x =>
 {
     x.BaseAddress = new Uri("http://localhost:5001/api/product/");
-}).AddTransientHttpErrorPolicy(policy => policy.WaitAndRetryAsync(3, _ => TimeSpan.FromSeconds(2)));
+}).AddPolicyHandler(circuitBreakerPolicy);
+
+
+IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .CircuitBreakerAsync(5, TimeSpan.FromSeconds(30), onBreak: (arg1, arg2) =>
+        {
+            Debug.WriteLine($"Circuit Breaker Status => On Break :");
+        }, onReset: () =>
+        {
+            Debug.WriteLine($"Circuit Breaker Status => On Reset :");
+        }, onHalfOpen: () =>
+        {
+            Debug.WriteLine($"Circuit Breaker Status => On Half Open :");
+        });
+}
+IAsyncPolicy<HttpResponseMessage> GetAdvancedCircuitBreakerPolicy()
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        // 30 saniyede gelen istek sayýsýnýn yüzde 10 u baþarýsýzsa devreye gir. 30 saniye sonra sýfýrla.
+        .AdvancedCircuitBreakerAsync(0.1,TimeSpan.FromSeconds(30),5,TimeSpan.FromSeconds(30), onBreak: (arg1, arg2) =>
+        {
+            Debug.WriteLine($"Circuit Breaker Status => On Break :");
+        }, onReset: () =>
+        {
+            Debug.WriteLine($"Circuit Breaker Status => On Reset :");
+        }, onHalfOpen: () =>
+        {
+            Debug.WriteLine($"Circuit Breaker Status => On Half Open :");
+        });
+}
 
 
 var app = builder.Build();
