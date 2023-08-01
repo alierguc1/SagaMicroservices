3fc8a80b2e66b90492db37f6edba66ad0f888694
Ali Ergüç <ergucali1@gmail.com>
2023-08-01 03:13:33 +0300

    .

---
+++ worker_services/SagaStateMachineWorkerService/Models/OrderStateInstance.cs
@@ -0,0 +1,15 @@
+﻿using Automatonymous;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SagaStateMachineWorkerService.Models
+{
+    public class OrderStateInstance : SagaStateMachineInstance
+    {
+        public Guid CorrelationId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
