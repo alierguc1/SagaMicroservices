bb1ed7e3cc446044214bda0743dce978d3629162
Ali Ergüç <ergucali1@gmail.com>
2023-08-09 17:19:38 +0300

    Create Product Controller and Created Endpoints.

---
+++ EventSourcing.API/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using EventSourcing.API.Command;
+using EventSourcing.API.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventSourcing.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("CreateProduct")]
+        public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto)
+        {
+            return Ok(await _mediator.Send(new CreateProductCommand() { CreateProductDto = createProductDto}));
+        }
+
+        [HttpPut("ChangeProductName")]
+        public async Task<IActionResult> ChangeProductName(ChangeProductNameDto changeProductNameDto)
+        {
+            return Ok(await _mediator.Send(new ChangeProductNameCommand() { ChangeProductNameDto = changeProductNameDto }));
+        }
+
+        [HttpPut("ChangeProductPrice")]
+        public async Task<IActionResult> ChangeProductPrice(ChangeProductPriceDto changeProductPriceDto)
+        {
+            return Ok(await _mediator.Send(new ChangeProductPriceCommand() { changeProductPriceDto = changeProductPriceDto }));
+        }
+
+        [HttpDelete("DeleteProduct/{Id}")]
+        public async Task<IActionResult> DeleteProduct(Guid Id)
+        {
+            return Ok(await _mediator.Send(new DeleteProductCommand() { Id = Id }));
+        }
+    }
+}
