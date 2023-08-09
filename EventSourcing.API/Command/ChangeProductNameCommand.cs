7e8e4411a7f7dc7364748f5d7567990dd3aad99e
Ali Ergüç <ergucali1@gmail.com>
2023-08-09 04:00:39 +0300

    Create All Request Command.

---
+++ EventSourcing.API/Command/ChangeProductNameCommand.cs
@@ -0,0 +1,10 @@
+﻿using EventSourcing.API.Dtos;
+using MediatR;
+
+namespace EventSourcing.API.Command
+{
+    public class ChangeProductNameCommand : IRequest
+    {
+        public ChangeProductNameDto ChangeProductNameDto { get; set; }
+    }
+}
---
+++ EventSourcing.API/Command/CreateProductCommand.cs
@@ -0,0 +1,13 @@
+﻿using EventSourcing.API.Dtos;
+using MediatR;
+
+namespace EventSourcing.API.Command
+{
+    public class CreateProductCommand : IRequest
+    {
+        public CreateProductDto CreateProduct { get; set; }
+
+
+
+    }
+}
