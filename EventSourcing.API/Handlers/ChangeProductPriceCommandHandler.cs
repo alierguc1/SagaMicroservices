6b45645771cb5319b79c81d6b687047fc7c01075
Ali Ergüç <ergucali1@gmail.com>
2023-08-09 17:10:14 +0300

    Create Command Handlers.

---
+++ EventSourcing.API/Handlers/ChangeProductPriceCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using EventSourcing.API.Command;
+using EventSourcing.API.EventStore;
+using MediatR;
+
+namespace EventSourcing.API.Handlers
+{
+    public class ChangeProductPriceCommandHandler : IRequestHandler<ChangeProductPriceCommand>
+    {
+        private readonly ProductStream _productStream;
+
+        public ChangeProductPriceCommandHandler(ProductStream productStream)
+        {
+            _productStream = productStream;
+        }
+        public async Task<Unit> Handle(ChangeProductPriceCommand request, CancellationToken cancellationToken)
+        {
+            _productStream.PriceChanged(request.changeProductPriceDto);
+            await _productStream.SaveAsync();
+            return Unit.Value;
+        }
+    }
+}
---
+++ EventSourcing.API/Handlers/DeleteProductCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using EventSourcing.API.Command;
+using EventSourcing.API.EventStore;
+using MediatR;
+
+namespace EventSourcing.API.Handlers
+{
+    public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand>
+    {
+        private readonly ProductStream _productStream;
+
+        public DeleteProductCommandHandler(ProductStream productStream)
+        {
+            _productStream = productStream;
+        }
+
+        public async Task<Unit> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            _productStream.Deleted(request.Id);
+            await _productStream.SaveAsync();
+            return Unit.Value;
+        }
+    }
+}
